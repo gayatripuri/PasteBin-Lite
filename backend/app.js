facbb51e22c6bddcfe3fbb18f5d8465974a76bda
Gayatri Puri <92669468+gayatripuri@users.noreply.github.com>
2026-01-29 15:27:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://pastebinlite-taupe.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"]
   })
